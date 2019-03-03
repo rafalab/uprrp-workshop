9c5f2980038d4b4a1a624dc44397f830886dd3a3
Rafael A. Irizarry <rairizarry@gmail.com>
2019-03-03 16:45:45 -0500

    adding content

--- README.md
+++ README.md
@@ -1,3 +1,35 @@
 # Big Data in Biology: From Genes to the Biosphere
 
-Material used in the classes and workshop by Rafael Irrizary
+Material used in the classes and workshop by Rafael Irizarry
+
+You can download a copy onto your computer using
+
+git clone
+
+or, if you don't have git, clicking...
+
+## Outline and learning objectives
+
+The course intermixes short lectures with assessments. The two main topics of this week are data visualization and data wrangling. For the assessments we will be answering biologically relevant questions and preparing publication ready figures using R code. The assessments will be carried out on a table with less than 500 rows, which we use to illustrate important principles that can then be extrapolated to "Big Data".
+
+1. Basic data manipulation. After a 15 minute lecture and 15 minute lab, students will be able to:
+
+    a. Identify if data is tidy or not.
+    b. Import a csv file into data frame in R.
+    c. Select, filter and change columns on data frame with __dplyr__ functions.
+    d. Stratify and summarize data with __dplyr__ functions.
+  
+  
+2. Data visualization principles. After a 30 minute lecutre and 30 minute lab students will be able to
+
+    a. Create publication ready figures using __ggplot__. 
+    b. Determine the more appropriate type of plot to convey a biological result.
+    c. Avoid commonly made plots that ofuscate or distract.
+  
+3. Data wrangling. The coding approach we present in the previous two sections work best with tidy data. In Biology it is common to receive data that is not in tidy format. In fact, often the majority of data analysis time is spent of _wrangling_ the data into a format that facilitates analysis. We will describe some of the tools that are available to do this.
+
+    a. Wrangle a jagged array stored in an excel file into a tidy data table.
+    b. Join tables to combine data from multiple datasets into one table.
+    
+
+
--- make-example-data.R
+++ make-example-data.R
@@ -3,45 +3,74 @@ dat <- read_csv("data/mouse.csv")
 ## adding simpons paradox
 dat <- mutate(dat, 
               fragment = recode(DNA, "1"="141G6", "2"="152F7", "3"="230E8", "4"="285E6")) %>%
-  mutate(fragment = ifelse(tg == 1, fragment, "No trisomy")) %>%
-  mutate(sex = ifelse(sex == 1, "Male", "Female"))
+  mutate(fragment = ifelse(tg == 1, fragment, "No trisomy")) 
 ## remove sex effect
 resids <- lm(weight ~ sex, data = dat)$resid
 resids <- resids/sd(resids)
-indexes <- with(dat,split(1:nrow(dat), list(fragment, sex)))
+indexes <- with(dat,split(1:nrow(dat), list(sex,fragment)))
 
 set.seed(2015)
 bp_sd <- 10
-bp_avg <- 125
+bp_avg_m <- 126
+bp_avg_f <- 124
 bp <- vector("numeric", nrow(dat))
 for(i in seq_along(indexes)){
+  bp_avg <- ifelse(grepl("0\\.",names(indexes)[i]), bp_avg_f, bp_avg_m)
   ind <- indexes[[i]]
   avg <- mean(resids[ind])
   ## create a negative correlation
-  new_avg <- bp_avg - 1*avg*bp_sd
+  new_avg <- bp_avg - 0.75*avg*bp_sd
   ## now create within group positive correlation
   bp[ind] <- new_avg + resids[ind] - mean(resids[ind]) + 
-    rnorm(length(ind), 0, 1)
-  print(c(length(ind), cor(bp[ind], dat$weight[ind])))
+    rnorm(length(ind), 0, 1.5)
+  #print(c(length(ind), cor(bp[ind], dat$weight[ind])))
 }
-print(cor(bp, dat$weight))
-
-dat <- select(dat, -bp)
-dat <- mutate(dat, bp = bp)
-
-dat %>% filter(sex == "Female") %>%
-  ggplot(aes(weight, bp)) + #, color = fragment)) + 
-  geom_point()
-
-dat %>% filter(sex == "Female") %>%
-  ggplot(aes(weight, bp, color = fragment)) + 
-  geom_point()
-
-dat %>% group_by(fragment, sex) %>%
-  summarize(avg = mean(weight)) %>% ungroup() %>%
-  spread(fragment, avg)
+dat <- mutate(dat, bp = round(bp, 1))
 
 
-### adding residual
-ind <- sample(which(dat$tg==0), 1)
-dat$weight[ind] <- dat$weight[ind]*100
+
+### jagged arrays
+indexes <- with(dat,split(1:nrow(dat), list(DNA)))
+
+n_max <- max(sapply(indexes, length))
+## first weight
+w_tabs <- lapply(seq(along=indexes), function(i){
+  ind <- indexes[[i]]
+  res <- select(dat, -c("bp","fragment","DNA")) %>% slice(ind)
+  frag <- c("141G6", "152F7", "230E8", "285E6")[as.numeric(names(indexes)[i])]
+  names(res) <- paste(frag, names(res), sep=".")
+  res <- mutate_all(res, as.character)
+  tmp <- matrix("", n_max-nrow(res), ncol(res))
+  colnames(tmp) <- colnames(res)
+  res <- bind_rows(res, as_data_frame(tmp))
+  res
+})
+weight <- do.call(cbind, w_tabs)
+
+## now bp
+## we add an extra line.
+n <- 15
+bp <- dat %>% select(c("line","bp")) %>% 
+  separate(line, c("line.1", "line.2", "line.3"), sep="-") %>%
+  mutate(line.1 = gsub("#", "", line.1)) %>%
+  bind_rows(data_frame(line.1 = rep("44", n), 
+                       line.2 = rep("22", n), 
+                       line.3 = as.character(1:n), 
+                       bp = rnorm(n, mean(dat$bp), sd(dat$bp)))) %>%
+  arrange(line.1, line.2)
+
+
+write.csv(weight, file = "data/weight.csv", quote = FALSE, row.names = FALSE)
+write.csv(bp, file = "data/blood-pressure.csv", quote = FALSE, row.names = FALSE)
+
+### add outlien then save RDA save the rda
+### adding outlier
+dat <- bind_rows(dat,
+                data_frame(DNA = 1, line = paste0("#85-12-",1:5), 
+                           tg = 0, sex = 0, age = 101,
+                           weight = 999, bp = 999, cage = 1))
+
+dat <- select(dat, DNA,line,tg,sex,age,weight,bp,cage)
+save(dat, file = "rdas/mouse.rda")
+
+
